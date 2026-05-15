91ad0730510b222c3bef97cb6a72f4d724879100
NOTAM <sawyerbobk563@gmail.com>
2026-05-15 12:24:06 -0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
-
-Once these are in GitHub, go to the Vercel Dashboard, select "New Project," and import this repo. It will detect it as a **Vite** project and deploy it for you! 
-
-**One Final Check:** Don't forget to add your Vercel link to the "Authorized JavaScript Origins" in your Google Cloud Console so the login works!
+// This is the entry point that connects our App component to the index.html file
+// We removed the .jsx extension from the import to help the bundler resolve the file path correctly
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+)
